8982b17d13487d832b6692ca64f0ae43a6f37195
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-05-03 12:06:16 +0500

    class abstarction

--- Abstract/Abstract/Program.cs
+++ Abstract/Abstract/Program.cs
@@ -10,6 +10,56 @@ namespace Abstract
     {
         static void Main(string[] args)
         {
+
+        }
+
+
+        public abstract class Animal
+        {
+            public string Name { get; set; }
+            public Animal()
+            {
+                
+            }
+
+            public virtual void Run()
+            {
+                Console.WriteLine("Animal is running");
+            }
+
+            public abstract void Run1();
+        }
+
+        class Dog : Animal
+        {
+            public override void Run1()
+            {
+                Console.WriteLine("Run 1 abstract Dog is running");
+            }
+            public override void Run()
+            {
+                Console.WriteLine("Dog is running ");
+            }
+        }
+
+        class Cat : Animal
+        {
+            public override void Run1()
+            {
+                Console.WriteLine("Run 1 abstract Cat is running");
+            }
+        }
+
+        class Lion : Animal
+        {
+            public override void Run1()
+            {
+                Console.WriteLine("Runr1 abstract : Lion is running");
+            }
+            public override void Run()
+            {
+                Console.WriteLine("Lion is running");
+            }
         }
     }
 }
